70ee8e2d9b9bd646183b3be75601277266617665
mherrerog <c3_manuel_herrero@yahoo.es>
2016-04-20 11:57:28 +0200

    Esquema BBDD

---
+++ BaseDatos/ASoftware_Comentario.sql
@@ -0,0 +1,49 @@
+-- MySQL dump 10.13  Distrib 5.7.9, for osx10.9 (x86_64)
+--
+-- Host: localhost    Database: ASoftware
+-- ------------------------------------------------------
+-- Server version	5.7.9
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Comentario`
+--
+
+DROP TABLE IF EXISTS `Comentario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Comentario` (
+  `idComentario` int(11) NOT NULL AUTO_INCREMENT,
+  `Publicacion_com` int(11) NOT NULL,
+  `Hora` varchar(5) NOT NULL,
+  `Fecha` varchar(8) NOT NULL,
+  `Usuario_com` int(11) NOT NULL,
+  `Contenido` text,
+  PRIMARY KEY (`idComentario`),
+  KEY `Usuario_com_idx` (`Usuario_com`),
+  KEY `Publicacion_com_idx` (`Publicacion_com`),
+  CONSTRAINT `Publicacion_com` FOREIGN KEY (`Publicacion_com`) REFERENCES `Publicacion` (`idPublicacion`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `Usuario_com` FOREIGN KEY (`Usuario_com`) REFERENCES `Usuario` (`idUsuarios`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-04-20 11:55:58
---
+++ BaseDatos/ASoftware_Compartir.sql
@@ -0,0 +1,44 @@
+-- MySQL dump 10.13  Distrib 5.7.9, for osx10.9 (x86_64)
+--
+-- Host: localhost    Database: ASoftware
+-- ------------------------------------------------------
+-- Server version	5.7.9
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Compartir`
+--
+
+DROP TABLE IF EXISTS `Compartir`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Compartir` (
+  `pk_usuario_c` int(11) NOT NULL,
+  `pk_publicacion_c` int(11) NOT NULL,
+  PRIMARY KEY (`pk_usuario_c`,`pk_publicacion_c`),
+  KEY `Publicacion_c_idx` (`pk_publicacion_c`),
+  CONSTRAINT `Publicacion_c` FOREIGN KEY (`pk_publicacion_c`) REFERENCES `Publicacion` (`idPublicacion`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `Usuario_c` FOREIGN KEY (`pk_usuario_c`) REFERENCES `Usuario` (`idUsuarios`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-04-20 11:55:58
