d2c36d1e792ec6a98f3ab9bec318d7cd5c2b8a44
Victorbp09 <vbeltran09@hotmail.com>
2016-05-10 08:17:51 +0200

    Se pueden eliminar publicaciones

---
+++ ArqSoftware/src/controller/DeletePub.java
@@ -0,0 +1,80 @@
+package controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import gateway.PublicacionDAO;
+
+/**
+ * Servlet implementation class MeGusta
+ */
+@WebServlet("/DeletePub")
+public class DeletePub extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeletePub() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		deletePublication(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	
+	/**
+	 * 
+	 */
+	private void deletePublication(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Obtener usuario
+		
+		String cookieName = "userId";
+		String userId = "";
+		Cookie[] cookies = request.getCookies();
+		if (cookies != null) 
+		{
+		    for(int i=0; i<cookies.length; i++) 
+		    {
+		        Cookie cookie = cookies[i];
+		        if (cookieName.equals(cookie.getName())) 
+		        {
+		        	userId = (cookie.getValue());
+		        }
+		    }
+		}
+		
+		int usuario = 1;
+		int publicacion = Integer.parseInt(request.getParameter("pub"));
+		
+		try {
+			PublicacionDAO.delete(publicacion);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		response.sendRedirect("/ArqSoftware/Social-Network/home.html");
+	}
+
+}
+
